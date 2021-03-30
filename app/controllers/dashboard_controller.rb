81d47f1824861b930a0d4774ae33b6b449ed44b0
Juan Carlos Guiza <juanc-guizar@unilibre.edu.co>
2021-03-29 20:33:20 -0500

    all styles of menus

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,8 +2,18 @@ class DashboardController < ApplicationController
     def inicio
         @name = "Cash Manager"
     end
+    def bolsillos
+        @name = "Cash Manager"
+    end
+    def graficas
+        @name = "Cash Manager"
+    end
+    def balances
+        @name = "Cash Manager"
+    end
     def historico
         @name = "Cash Manager"
     end
+    
 
 end
